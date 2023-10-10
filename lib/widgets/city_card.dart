edd6f9e72c4b279698ee9a4e33e5b427817d0823
Achmad Dinofaldi Firmansyah <aldi.novaldi.frmnsyah@gmail.com>
2023-10-10 16:43:26 +0700

    lib/widgets/city_card.dart

--- lib/widgets/city_card.dart
+++ lib/widgets/city_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:houseapp/pages/space_page.dart';
 import 'package:houseapp/theme.dart';
 
 class CityCard extends StatelessWidget {
@@ -10,54 +11,64 @@ class CityCard extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(18),
-      child: Container(
-        height: 150,
-        width: 120,
-        color: const Color(0xffF6F7F8),
-        child: Column(
-          children: [
-            Stack(
-              children: [
-                Image.asset(
-                  imageUrl,
-                  width: 120,
-                  height: 102,
-                  fit: BoxFit.cover,
-                ),
-                if (isPopular) Align(
-                  alignment: Alignment.topRight,
-                  child: Container(
-                    width: 50,
-                    height: 30,
-                    decoration: BoxDecoration(
-                      color: purpleColor,
-                      borderRadius: const BorderRadius.only(
-                        bottomLeft: Radius.circular(36),
-                      ),
-                    ),
-                    child: Center(
-                      child: Image.asset(
-                        'assets/icon_star.png',
-                        width: 22,
-                        height: 22,
-                      ),
-                    ),
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context){
+            return SpacePage(city: cityName);
+          }),
+        );
+      },
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(18),
+        child: Container(
+          height: 150,
+          width: 120,
+          color: const Color(0xffF6F7F8),
+          child: Column(
+            children: [
+              Stack(
+                children: [
+                  Image.asset(
+                    imageUrl,
+                    width: 120,
+                    height: 102,
+                    fit: BoxFit.cover,
                   ),
-                ) else Container()
-              ],
-            ),
-            const SizedBox(
-              height: 11,
-            ),
-            Text(
-              cityName,
-              style: blackTextStyle.copyWith(
-                fontSize: 16,
+                  if (isPopular) Align(
+                    alignment: Alignment.topRight,
+                    child: Container(
+                      width: 50,
+                      height: 30,
+                      decoration: BoxDecoration(
+                        color: purpleColor,
+                        borderRadius: const BorderRadius.only(
+                          bottomLeft: Radius.circular(36),
+                        ),
+                      ),
+                      child: Center(
+                        child: Image.asset(
+                          'assets/icon_star.png',
+                          width: 22,
+                          height: 22,
+                        ),
+                      ),
+                    ),
+                  ) else Container()
+                ],
               ),
-            ),
-          ],
+              const SizedBox(
+                height: 11,
+              ),
+              Text(
+                cityName,
+                style: blackTextStyle.copyWith(
+                  fontSize: 16,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
